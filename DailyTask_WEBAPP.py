7a5a73dd615bfd780a46d313baee1140f884706d
Nicola Chimenti <nicola.chimenti.work@gmail.com>
2024-06-27 23:10:20 +0200

    Update DailyTask_WEBAPP.py
    Graphical improvement

--- DailyTask_WEBAPP.py
+++ DailyTask_WEBAPP.py
@@ -18,6 +18,7 @@ import math
 
 # The day of the exam
 st.write("# DETAILS ABOUT THE EXAM")
+st.write("### Let's see when we are going to do it 📅")
 year = 2024
 year1 = st.number_input("Insert the year: ", value = 2024, step=1, min_value=2024)
 year = int(year1)   #,value = 2024, step = 1)#value = datetime.today().year)  # Insert the year of the exam
@@ -25,7 +26,7 @@ month1 = st.slider("Month: ", 1,12,1)
 day1 = st.slider("Day: ", 1,31,1)
 
 ft = date(year,month1,day1)
-pages = st.number_input("Number of pages: ", step=1)
+pages = st.number_input("Number of pages 📖: ", step=1)
 
 # Days left?
 td = datetime.date.today()       # Actual datetime
@@ -34,8 +35,8 @@ diff = ft - td   # How many days are left for the exam?
 st.write("\n")
 if(diff.days > 0):   # If there is a POSITIVE amount of days between exams and today
   st.write("# STARTING OF THE OUTPUTS")
-  st.write ("\n### Days left for the exam:\t",diff.days)                # Days left, output print
-  st.write("\n### Average pages per day:\t",math.ceil(pages/(diff.days-1)))  # Round at the next integer number
+  st.write ("\n### Days left ⏰ for the exam:\t",diff.days)                # Days left, output print
+  st.write("\n### Average pages 📝 per day:\t",math.ceil(pages/(diff.days-1)))  # Round at the next integer number
 
   st.write("\n\n\n")
   st.write("# Do you want to have the time to do a review or to relax?")
@@ -46,7 +47,7 @@ if(diff.days > 0):   # If there is a POSITIVE amount of days between exams and t
   while diff.days > possibilities:   # Calculating the possible scenarios
     possibilities += increment
     if(diff.days-1-possibilities > 0):   # Avoiding 0 divisions or other common errors
-      st.write(f"\nTo have {possibilities} days off, you should study: {round(pages/(diff.days-1-possibilities),1)} pages per day")
+      st.write(f"\nTo have {possibilities} days off, you should study: {round(pages/(diff.days-1-possibilities),1)} pages 📖 per day")
     else:
       break  # End of the cycle in case of 0 division or negative numbers of days
 
