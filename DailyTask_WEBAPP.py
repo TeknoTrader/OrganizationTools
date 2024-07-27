16b08af65b561cf036f97888a1e7f70a18d17eda
Nicola Chimenti <nicola.chimenti.work@gmail.com>
2024-07-27 15:38:11 +0200

    Update DailyTask_WEBAPP.py

--- DailyTask_WEBAPP.py
+++ DailyTask_WEBAPP.py
@@ -68,11 +68,11 @@ if(diff.days > 0):   # If there is a POSITIVE amount of days between exams and t
   # Comments to clarify
   for i in range(len(days_number)):
     if (len(days_number) > 10):
-      st.write(f"\nTo have {i} days off, you should study: {pages_number[i] * increment} pages 📖 per day")
+      st.write(f"\nTo have {i * increment} days off, you should study: {pages_number[i]} pages 📖 per day")
     elif (len(days_number) > 5):
-      st.write("\n### To have ", i, " days off, you should study: ", pages_number[i] * increment, " pages 📖 per day")
+      st.write("\n### To have ", i * increment, " days off, you should study: ", pages_number[i], " pages 📖 per day")
     else:
-      st.write("\n# To have ", i, " days off, you should study: ", pages_number[i] * increment, " pages 📖 per day")
+      st.write("\n# To have ", i * increment, " days off, you should study: ", pages_number[i], " pages 📖 per day")
 
 else:
     st.write("\n")
