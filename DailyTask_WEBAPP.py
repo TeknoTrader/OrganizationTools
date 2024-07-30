6545313ad6888ba5af59a91d205b40ac7b0775be
Nicola Chimenti <nicola.chimenti.work@gmail.com>
2024-07-30 20:37:20 +0200

    Update DailyTask_WEBAPP.py

--- DailyTask_WEBAPP.py
+++ DailyTask_WEBAPP.py
@@ -40,7 +40,7 @@ year = 2024
 year1 = st.number_input("Insert the year: ", min_value = td.year, value = td.year, step=1)
 year = int(year1)   #,value = 2024, step = 1)#value = datetime.today().year)  # Insert the year of the exam
 month1 = st.slider("Month: ", min_value=1, max_value=12, value = td.month, step=1)
-day1 = st.slider("Day: ", min_value=1, max_value=12, value = td.day, step=1)
+day1 = st.slider("Day: ", min_value=1, max_value = 31, value = td.day, step=1)
 
 ft = date(year,month1,day1)
 st.write("### How much do you have to study/work 📖?")
