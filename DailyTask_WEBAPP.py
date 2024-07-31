f6feece5fb118a38cec65fb45bf5fe155c6783dc
Nicola Chimenti <nicola.chimenti.work@gmail.com>
2024-07-31 20:52:25 +0200

    Update DailyTask_WEBAPP.py

--- DailyTask_WEBAPP.py
+++ DailyTask_WEBAPP.py
@@ -131,6 +131,7 @@ else:
                           ["Normal","Extended"]
                 )
                 if rad == "Extended":
+                          df.style.format(precision=0)
                           st.markdown(df.style.hide(axis="index").to_html(), unsafe_allow_html=True)
                 else:
                           st.dataframe(df, hide_index=True)  #another way to hide column: st.dataframe(df.set_index(df.columns[0]))
