13be0c7cfb5116d6d49f9612755056ddb22a00c5
Nicola Chimenti <nicola.chimenti.work@gmail.com>
2025-10-14 17:34:32 +0200

    Update DailyTask_WEBAPP.py

--- DailyTask_WEBAPP.py
+++ DailyTask_WEBAPP.py
@@ -17,10 +17,11 @@ import calendar
 
 # Some information about me
 st.sidebar.write("# Who built this web application?")
-st.sidebar.write("My name is Nicola Chimenti.\nI'm currently pursuing a degree in \"Digital Economics\" and I love finance, programming and Data Science")
-st.sidebar.image("https://i.postimg.cc/7LynpkrL/Whats-App-Image-2024-07-27-at-16-36-44.jpg") # caption="My name is Nicola Chimenti.\nI'm currently pursuing a degree in \"Digital Economics\" and I love finance, programming and Data Science" , use_column_width=True)
+st.sidebar.write("My name is Nicola Chimenti and I am a Business Analyst and MQL4.\nI have a degree in \"Digital Economics\" and I love finance, programming and Data Science")
+st.sidebar.image("https://i.postimg.cc/7LynpkrL/Whats-App-Image-2024-07-27-at-16-36-44.jpg") # caption="My name is Nicola Chimenti and I am a Business Analyst and MQL4." , use_column_width=True)
 st.sidebar.write("\n# CONTACT ME")
 st.sidebar.write("### ◾ [LinkedIn](https://www.linkedin.com/in/nicolachimenti?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app)")
+st.sidebar.write("### ◾ [Fiverr profile](https://www.fiverr.com/sellers/teknonicola/)
 st.sidebar.write("### ◾ Email: nicola.chimenti.work@gmail.com")
 st.sidebar.write("\n# RESOURCES")
 st.sidebar.write("◾ [GitHub Profile](https://github.com/TeknoTrader)")
